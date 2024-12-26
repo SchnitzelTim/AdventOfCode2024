de528056212dec0fca752fed7f9147bf490f0033
Tim <TBWetter@gmail.com>
2024-12-26 18:53:29 +0100

    Completed the solution for day 9.

--- 09/my_solution.py
+++ 09/my_solution.py
@@ -40,4 +40,87 @@ while idxFront < idxBack:
 			idxFront += 1
 			iState = 0
 	elif iState == 1:
+		if lRearrangedDiskLayout[idxBack] is None:
+			idxBack -= 1
+			iState = 1
+		else:
+			if lRearrangedDiskLayout[idxFront] is None:
+				lRearrangedDiskLayout[idxFront] = lRearrangedDiskLayout[idxBack]
+				lRearrangedDiskLayout[idxBack] = None
+				idxBack -= 1
+				idxFront += 1
+				iState = 0
+			else:
+				raise ValueError('The front block must be None at this point')
+	else:
+		raise ValueError('Unexpected state')
 
+iChecksum = 0
+for idxBlock, iBlockValue in enumerate(lRearrangedDiskLayout):
+	if isinstance(iBlockValue, int):
+		iChecksum += idxBlock * iBlockValue
+
+print(f'The checksum after rearranging the disk layout is {iChecksum}.')
+
+lRearrangedDiskLayout = list(lDiskLayout)
+idxBack = len(lRearrangedDiskLayout) - 1
+iFileID = None
+idxFileEnd = None
+idxFileStart = None
+idxFront = 0
+idxFreeSectionStart = None
+idxFreeSectionEnd = None
+iState = 0
+lWasMoved = [False] * len(lRearrangedDiskLayout)
+while idxBack > 0:
+	if iState == 0: # Find the end of a file that was not moved yet
+		if lRearrangedDiskLayout[idxBack] is None or lWasMoved[idxBack]:
+			idxBack -= 1
+			print('Back', idxBack)
+			iState = 0
+		else:
+			iFileID = lRearrangedDiskLayout[idxBack]
+			idxFileEnd = idxBack
+			iState = 1
+	elif iState == 1: # Find the start of the file
+		if lRearrangedDiskLayout[idxBack] == iFileID:
+			idxFileStart = idxBack
+			idxBack -= 1
+			print('Back', idxBack)
+			iState = 1
+		else:
+			idxFront = 0
+			iState = 2
+	elif iState == 2: # Find next free block
+		if lRearrangedDiskLayout[idxFront] is None:
+			idxFreeSectionStart = idxFront
+			iState = 3
+		else:
+			idxFront += 1
+			if idxFront < idxFileStart:
+				iState = 2
+			else:
+				iState = 0
+	elif iState == 3: # Find the end of the free block
+		if lRearrangedDiskLayout[idxFront] is None:
+			idxFreeSectionEnd = idxFront
+			idxFront += 1
+			iState = 3
+		else:
+			if idxFreeSectionEnd - idxFreeSectionStart + 1 >= idxFileEnd - idxFileStart + 1:
+				for idxFront in range(idxFileEnd - idxFileStart + 1):
+					lRearrangedDiskLayout[idxFreeSectionStart + idxFront] = iFileID
+					lWasMoved[idxFreeSectionStart + idxFront] = True
+					lRearrangedDiskLayout[idxFileStart + idxFront] = None
+				iState = 0
+			else:
+				iState = 2
+	else:
+		raise ValueError('Invalid state')
+
+iChecksum = 0
+for idxBlock, iBlockValue in enumerate(lRearrangedDiskLayout):
+	if isinstance(iBlockValue, int):
+		iChecksum += idxBlock * iBlockValue
+
+print(f'The checksum after rearranging the disk layout is {iChecksum}.')
